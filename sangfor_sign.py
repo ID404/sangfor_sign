07312126313e1722992a5a34668fcbefb6d1ebab
ID404 <ljc8903@126.com>
2023-12-19 18:44:02 +0800

    Update sangfor_sign.py

--- sangfor_sign.py
+++ sangfor_sign.py
@@ -1,4 +1,5 @@
-#此程序主要为签到并领取S豆
+
+#此程序主要为登陆深信服官网签到并领取S豆
 #定位网页元素可以通过chrome 右键检查，复制完整xpath
 #linux下需要先安装好firefox
 #yum install firefox
@@ -13,119 +14,180 @@ from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
 import datetime
+import requests
+import re
 
-def print_with_timestamp(text):
+sangfor_username = "admin"
+sangfor_passowrd = "admin"
+alarm_url = "https://api.day.app/RfDX/sangfor_sign/"
+alarm_day = 1
+
+def print_with_timestamp(text,text1=None):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"[{formatted_time}] {text}")
+    if text1 is not None: # 检查传入的参数是否为 None
+        if text1: # 如果传入了网址参数并且不为零
+            print(f"[{formatted_time}] {text} {text1}")
+        else: # 如果传入了网址参数但为零
+            print(f"[{formatted_time}] {text} 0")
+    else: # 如果没有传入网址参数
+        print(f"[{formatted_time}] {text}")
 
-def login_and_claim_rewards():
-    options = Options()
-    options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/95.0")
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
-    wait = WebDriverWait(driver, 10)
-    main_window = driver.current_window_handle
+
+options = Options()
+options.headless = True
+options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/95.0")
+options.add_argument("--width=0")
+options.add_argument("--headless")
+options.add_argument("--height=0")
+driver = webdriver.Firefox(options=options)
+
+
+wait = WebDriverWait(driver, 10)
+
+
+# 打开需要登录的网页
+print_with_timestamp("正在打开网页!")
+driver.get("https://bbs.sangfor.com.cn/plugin.php?id=info:index#?orderby=dateline&type=index&page=1&init")
+print_with_timestamp("等待5秒")
+time.sleep(5)
+main_window = driver.current_window_handle
+print_with_timestamp("点登录的按钮")
+login_button1 = driver.find_element(By.CSS_SELECTOR, '#header > div.container > div > div.col-xs-4.fix-topbar-options > ul > li.header-login-btn > a')
+login_button1.click()
+
+print_with_timestamp("点验证码登录")
+VC_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[11]/div/div/root/div[1]/div/div[1]/ul/li[2]/a")))
+VC_button.click()
+
+print_with_timestamp("点验账号登录")
+sigin_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[11]/div/div/root/div[1]/div/form/div[6]/a[1]")))
+sigin_button.click()
+
+
+
+# 输入用户名和密码
+print_with_timestamp("输入用户名密码")
+username_input = driver.find_element(By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[1]/input[2]")
+password_input = driver.find_element(By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[2]/input")
+username_input.send_keys(sangfor_username)
+password_input.send_keys(sangfor_passowrd)
+
+
+# 点击登录按钮
+print_with_timestamp("点击登录")
+login_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[4]/button")))
+login_button.click()
+time.sleep(5)
+
+try:
+    print_with_timestamp("尝试领取每日福利")
+    FL_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div[2]/div/div")))
+    FL_button.click()
+    time.sleep(10)
+except TimeoutException:
+    print("已领取每日福利或领取错误")
+
+try:
+    print_with_timestamp("点击每日签到")
+    QD_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div > div.col-xs-3.components-info > div.sign-btn-wrap > a > span.sign-info")))
+    QD_button.click()
+    time.sleep(5)
+except TimeoutException:
+    print_with_timestamp("点击每日签到")
+    QD_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div > div.col-xs-3.components-info > div.sign-btn-wrap > a > span.sign-info.has_sign")))
+    QD_button.click()
+    time.sleep(5)
+
+print_with_timestamp("点击去抽奖")
+QCJ_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.modal.fade.ng-isolate-scope.in > div > div > div.sign_share.text-333.ng-scope > div:nth-child(2) > div.sign-info-content > div.sign_share-inner > p.mt10 > a")))#
+QCJ_button.click()
+time.sleep(8)
+
+#获取所有窗口的句柄，并切换到新窗口
+windows = driver.window_handles
+for window in windows:
+    if window != main_window:
+        driver.switch_to.window(window)
+        break
+
+time.sleep(8)
+
+
+
+#获取可抽奖次数
+choujiang_time_text = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div.row.mt10 > div.col-md-9 > div > div > div.text-center.text-white > h4:nth-child(2) > span:nth-child(3)")))#
+choujiang_time_text1 = choujiang_time_text.text
+match = re.search(r'(\d+)', choujiang_time_text1)
+print_with_timestamp(match)
+choujiang_time = int(match.group())
+print_with_timestamp("当前可抽奖次数:",choujiang_time)
+
+
+while choujiang_time > 0:
+    print_with_timestamp("剩余可抽奖次数:",choujiang_time)
     try:
-        # 打开需要登录的网页
-        print_with_timestamp("正在打开网页!")
-        driver.get("https://bbs.sangfor.com.cn/plugin.php?id=info:index#?orderby=dateline&type=index&page=1&init")
-        print_with_timestamp("等待5秒")
+        print_with_timestamp("点击抽奖")
+        CJ_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div.row.mt10 > div.col-md-9 > div > div > div.raffle-con-btn")))#
+        CJ_button.click()
         time.sleep(5)
-
-        # 点击登录的按钮
-        print_with_timestamp("点登录的按钮")
-        login_button1 = driver.find_element(By.CSS_SELECTOR, '#header > div.container > div > div.col-xs-4.fix-topbar-options > ul > li.header-login-btn > a')
-        login_button1.click()
-
-        # 点验证码登录
-        print_with_timestamp("点验证码登录")
-        VC_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[11]/div/div/root/div[1]/div/div[1]/ul/li[2]/a")))
-        VC_button.click()
-
-        # 点验账号登录
-        print_with_timestamp("点验账号登录")
-        sigin_button = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[11]/div/div/root/div[1]/div/form/div[6]/a[1]")))
-        sigin_button.click()
-
-        # 输入用户名和密码
-        print_with_timestamp(" ")
-        username_input = driver.find_element(By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[1]/input[2]")
-        password_input = driver.find_element(By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[2]/input")
-        username_input.send_keys("admin")
-        password_input.send_keys("123456")
-
-        # 点击登录按钮
-        print_with_timestamp("点击登录")
-        login_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[11]/div/div/root/div[1]/div/form/div[4]/button")))
-        login_button.click()
-        time.sleep(5)
-
-        try:
-            print_with_timestamp("尝试领取每日福利")
-            FL_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[3]/div/div/div/div[2]/div[2]/div/div")))
-            FL_button.click()
-            time.sleep(10)
-        except TimeoutException:
-            print_with_timestamp("已领取每日福利或领取超时")
-
-        try:            
-            QD_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[5]/div/div[2]/div[2]/a/span[1]")))
-            QD_button.click()
-            print_with_timestamp("点击每日签到")
-            time.sleep(5)
-        except TimeoutException:
-            print_with_timestamp("每日签到超时")
-
-        try: 
-            print_with_timestamp("点击去抽奖")
-            QCJ_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[10]/div/div/div[2]/div[2]/div[1]/div[1]/p[1]/a")))#
-            QCJ_button.click()
-            time.sleep(5)
-        except TimeoutException:
-            print_with_timestamp("点击去抽奖超时")
-
-        #获取所有窗口的句柄，并切换到新窗口
-        windows = driver.window_handles
-        for window in windows:
-            if window != main_window:
-                driver.switch_to.window(window)
-                break
-        
-        time.sleep(5)
-        
-        try: 
-            print_with_timestamp("点击抽奖")
-            CJ_button = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[4]/div[2]/div[1]/div/div/div[1]")))#
-            CJ_button.click()
-            time.sleep(5)
-        except TimeoutException:
-            print_with_timestamp("点击抽奖超时")
-
-
-        rewawrd_s_dou = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[10]/div/div/div/h3/span")))
-        text1 = rewawrd_s_dou.text
-        text = "本次抽中" + text1 + "S豆"
-        print_with_timestamp(text)
-        time.sleep(5)
-
-        S_dou = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[5]/div[2]/div[1]/div/div/div[3]/h4[1]/span[4]")))
-        text2 = S_dou.text
-        text = "当前S豆总数：" + text2
-        print_with_timestamp(text)
-
-        time.sleep(10)
-
     except TimeoutException:
-        print_with_timestamp("程序运行超时")  
-    except KeyboardInterrupt:  
-        print_with_timestamp(" ")
-        print_with_timestamp("手动中断程序运行")
-    finally:
-        # 关闭浏览器
-        print_with_timestamp("关闭浏览器")
-        driver.quit()
-        print_with_timestamp("签到结束")
+        print_with_timestamp("点击抽奖超时")
+    time.sleep(8)
+    try:
+        rewawrd_s_dou = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.modal.fade.ng-isolate-scope.in > div > div > div > h3 > span")))#
+        text = rewawrd_s_dou.text
+        print_with_timestamp("本次抽中S豆数量：",text)
+        time.sleep(5)
+    except TimeoutException:
+        print_with_timestamp("获取抽中S豆总数超时")
+    time.sleep(10)
+    try:
+        print_with_timestamp("点击继续抽奖")
+        jixuchoujian = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div.row.mt10 > div.col-md-9 > div > div > div.raffle-con-btn")))#
+        time.sleep(5)
+    except TimeoutException:
+        print_with_timestamp("点击继续抽奖超时")
+    time.sleep(10)
+    try:
+        S_dou = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div.row.mt10 > div.col-md-9 > div > div > div.text-center.text-white > h4:nth-child(2) > span:nth-child(4)")))
+        text = "当前S豆总数：" + S_dou.text
+        print_with_timestamp(text)
+    except TimeoutException:
+        print_with_timestamp("获取当前S豆总数超时")
 
-if __name__ == "__main__":
-    login_and_claim_rewards()
+    time.sleep(10)
+    choujiang_time -= 1
+
+try:
+    S_dou = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"body > div.container.ng-scope > div.row.mt10 > div.col-md-9 > div > div > div.text-center.text-white > h4:nth-child(2) > span:nth-child(4)")))
+    text = "当前S豆总数：" + S_dou.text
+    print_with_timestamp(text)
+except TimeoutException:
+    text = "无法获取当前S豆数量"
+    print_with_timestamp("获取当前S豆总数超时")
+
+
+#发送通知
+current_date = datetime.date.today()
+if current_date.day == alarm_day:
+    # 指定的URL
+    url = alarm_url + text
+    # 发送HTTP GET请求访问URL
+    response = requests.get(url)
+    
+    # 输出访问结果
+    if response.status_code == 200:
+        print_with_timestamp("通知发送成功")
+        print_with_timestamp(text)
+    else:
+        print_with_timestamp("通知发送失败")
+        print_with_timestamp(text)
+else:
+    print_with_timestamp("当前日期不是每月1号")
+
+
+# 关闭浏览器
+print_with_timestamp("关闭浏览器")
+driver.quit()
+print_with_timestamp("退出程序")
